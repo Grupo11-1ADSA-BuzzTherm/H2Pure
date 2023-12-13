14f7502cc673116f027cedf7497fc591e8d53989
LukaTamazato <luka.tamazato@sptech.school>
2023-12-13 11:28:52 -0300

    Adicionando os inserts nas tabelas

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -44,4 +44,87 @@ create table leitura (
 	FOREIGN KEY (fkEsteira) REFERENCES esteira(id)
 );
 
+-- Inserir empresas
+INSERT INTO empresa (razaoSocial, cnpj) VALUES
+('Tecnologia Inovadora Ltda', '12345678901234'),
+('Soluções Empresariais S.A.', '56789012345678'),
+('Logística Eficiente Ltda', '90123456789012');
+
+-- Inserir usuários associados a empresas
+-- Para a Tecnologia Inovadora Ltda
+INSERT INTO usuario (nome, email, senha, fkEmpresa) VALUES
+('João Silva', 'joao.silva@email.com', 'senha123', 1),
+('Ana Silva', 'ana.silva@email.com', 'senha456', 1);
+
+-- Para a Soluções Empresariais S.A.
+INSERT INTO usuario (nome, email, senha, fkEmpresa) VALUES
+('Maria Oliveira', 'maria.oliveira@email.com', 'senha789', 2),
+('Pedro Oliveira', 'pedro.oliveira@email.com', 'senha101', 2),
+('Fernanda Oliveira', 'fernanda.oliveira@email.com', 'senha112', 2);
+
+-- Para a Logística Eficiente Ltda
+INSERT INTO usuario (nome, email, senha, fkEmpresa) VALUES
+('Carlos Santos', 'carlos.santos@email.com', 'senha131', 3),
+('Camila Santos', 'camila.santos@email.com', 'senha141', 3);
+
+-- Inserir esteiras associadas a empresas
+-- Para a Tecnologia Inovadora Ltda
+INSERT INTO esteira (setor, fkEmpresa) VALUES
+('Setor 21', 1),
+('Setor 22', 1),
+('Setor 23', 1);
+
+-- Para a Soluções Empresariais S.A.
+INSERT INTO esteira (setor, fkEmpresa) VALUES
+('Setor A', 2),
+('Setor B', 2),
+('Setor C', 2),
+('Setor D', 2),
+('Setor E', 2);
+
+-- Para a Logística Eficiente Ltda
+INSERT INTO esteira (setor, fkEmpresa) VALUES
+('Setor 0', 3),
+('Setor 1', 3);
+
+-- Inserir leituras associadas a esteiras
+
+-- Para a Empresa do João Silva (Esteiras 1, 2, 3)
+INSERT INTO leitura (fkEsteira, apta) VALUES
+(1, 1),  -- Leitura válida na Esteira 1
+(1, 0),  -- Leitura inválida na Esteira 1
+(1, 1),  -- Leitura válida na Esteira 1
+(1, 1)  -- Leitura válida na Esteira 1
+;
+
+-- Para a Empresa da Maria Oliveira (Esteiras 4, 5, 6, 7, 8)
+INSERT INTO leitura (fkEsteira, apta) VALUES
+(4, 1),  -- Leitura válida na Esteira 4
+(4, 0),  -- Leitura inválida na Esteira 4
+(4, 1),  -- Leitura válida na Esteira 4
+(4, 1),  -- Leitura válida na Esteira 4
+
+(5, 1),  -- Leitura válida na Esteira 5
+(5, 1),  -- Leitura válida na Esteira 5
+(5, 0),  -- Leitura inválida na Esteira 5
+(5, 1)  -- Leitura válida na Esteira 5
+;
+
+-- Para a Empresa do Carlos Santos (Esteiras 9, 10)
+INSERT INTO leitura (fkEsteira, apta) VALUES
+(9, 1),  -- Leitura válida na Esteira 9
+(9, 1),  -- Leitura válida na Esteira 9
+(9, 1),  -- Leitura válida na Esteira 9
+(9, 0),  -- Leitura inválida na Esteira 9
+
+(10, 1), -- Leitura válida na Esteira 10
+(10, 1), -- Leitura válida na Esteira 10
+(10, 1), -- Leitura válida na Esteira 10
+(10, 1) -- Leitura válida na Esteira 10
+;
+
+select * from empresa;
+select * from usuario;
+select * from esteira;
+select * from leitura;
 
